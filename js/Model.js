c122e0c25ea69e6bbdf61677cfaf057af63c29af
MIHMahmudEli <mdmohsin11222@gmail.com>
2026-02-18 22:55:26 +0600

    feat: enable ramadan mode by default

--- js/Model.js
+++ js/Model.js
@@ -11,7 +11,7 @@ class RoutineModel {
         this.isExplorerMode = false;
         this.focusMode = false;
         this.twentyFourHourMode = false;
-        this.ramadanMode = false;
+        this.ramadanMode = true;
         // Check local storage FIRST (for admin persistence), default to false
         this.ramadanFeatureEnabled = localStorage.getItem('routine-pro-ramadan-admin-feature') === 'true';
         this.globalRamadanMap = null;
