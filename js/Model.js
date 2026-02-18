b70fc0e7c189f592170898c5c2731f43ba64fcaf
MIHMahmudEli <mdmohsin11222@gmail.com>
2026-02-18 23:14:36 +0600

    feat: rename 2hr gap filter to 'Min Gaps (Extra Compact)'

--- js/Model.js
+++ js/Model.js
@@ -196,6 +196,17 @@ class RoutineModel {
 
             // Apply strict filter: only show routines with <= 5h 40m (340 mins) of waiting time
             this.possibleRoutines = this.possibleRoutines.filter(r => this.calculateGaps(r) <= 340);
+        } else if (sortBy === 'max2hr') {
+            // First calculate and filter
+            this.possibleRoutines = this.possibleRoutines.filter(r => this.calculateGaps(r) <= 120);
+
+            // Then sort by gap amount
+            this.possibleRoutines.sort((a, b) => {
+                const gapsA = this.calculateGaps(a);
+                const gapsB = this.calculateGaps(b);
+                if (gapsA !== gapsB) return gapsA - gapsB;
+                return this.countDays(a) - this.countDays(b);
+            });
         }
 
         return this.possibleRoutines;
