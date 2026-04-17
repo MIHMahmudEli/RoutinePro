0efdc076b829a7e5a505466ce5e1c78de48b7c09
MIHMahmudEli <mdmohsin11222@gmail.com>
2026-04-17 18:32:24 +0600

    fix: Update decommissioned Groq vision model to llama-3.2-90b-vision-preview

--- js/Controller.js
+++ js/Controller.js
@@ -1715,7 +1715,7 @@ class RoutineController {
                             'Content-Type': 'application/json'
                         },
                         body: JSON.stringify({
-                            model: "llama-3.2-11b-vision-preview",
+                            model: "llama-3.2-90b-vision-preview",
                             messages: [
                                 {
                                     role: "user",
