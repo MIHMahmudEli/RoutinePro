55cb09f7b3ee2403ac7c49a7da3227d2ce656a69
MIHMahmudEli <mdmohsin11222@gmail.com>
2026-04-18 01:49:20 +0600

    Deployment Fix: Reorganize to professional public/ structure for Vercel compatibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./js/**/*.js",
+    "./public/index.html",
+    "./public/js/**/*.js",
     "./api/**/*.js"
   ],
   theme: {
